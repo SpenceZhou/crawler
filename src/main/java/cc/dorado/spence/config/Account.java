5ccae0494ae341a1bf5f72e7cf7ae6b0fe122306
zhoushuaipeng <zsp@dorado.cc>
2015-10-09 17:18:08 +0800

    账户信息读取

---
+++ src/main/java/cc/dorado/spence/config/Account.java
@@ -0,0 +1,75 @@
+package cc.dorado.spence.config;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.lang3.StringUtils;
+
+import cc.dorado.spence.util.SpiderLog;
+
+import com.google.common.collect.Maps;
+
+/**
+ * Project:spence
+ * FileName:Account.java
+ * PackageName:cc.dorado.spence.config
+ * Date:2015年7月7日
+ * Copyright (C) 2015, zsp@dorado.cc All rights reserved.
+ *
+ * ClassName:Account<br>
+ * Function:模拟登录用户
+ * 
+ * @auhter Spence
+ * @version 1.0
+ * @since JDK 1.7
+ */
+public class Account {
+	/**
+	 * accountPath:account路径
+	 */
+	public static final String accountPath = "src/main/java/account";
+	/**
+	 * sinaWeibo:新浪微博用户
+	 */
+	public static Map<String, String> sinaWeibo = new HashMap<String, String>();
+	
+	static{
+		sinaWeibo = readAccount(accountPath);
+	}
+
+	/**
+	 * readAccount:读取账户文件
+	 * @param accountPath
+	 * @return
+	 * @return Map<String,String>
+	 */
+	@SuppressWarnings("resource")
+	public static Map<String, String> readAccount(String accountPath){
+		Map<String, String> map = Maps.newHashMap();
+		File account = new File(accountPath);
+		if(!account.exists()){
+			SpiderLog.log(Account.class).warn("账户文件不存在");
+			return map;
+		}
+		String user = "";//用户
+		try {
+			FileReader fileReader = new FileReader(account);
+			BufferedReader reader = new BufferedReader(fileReader);
+			while((user=reader.readLine())!=null){
+				String username = StringUtils.substring(user, 0, user.indexOf(","));
+				String password = StringUtils.substring(user, user.indexOf(",")+1);
+				map.put(username, password);
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return map;
+	}
+}
